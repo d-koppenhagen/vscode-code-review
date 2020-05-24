68ae1db96d5d2535ea226e938c3c67c7656084cb
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-05-24 12:42:53 +0200

    feat: apply line references in built URL
    If multiple selection in VSCode were made in one file, the first selection line range is applied in the URL.
    
    This will e.g. link to the correct lines in  a github / gitlab URL
    
    closes #11

--- src/review-comment.ts
+++ src/review-comment.ts
@@ -16,12 +16,20 @@ export class ReviewCommentService {
   async addComment(comment: ReviewComment) {
     this.checkFileExists();
     let selections = '';
+    let startAnker: number | undefined = undefined;
+    let endAnker: number | undefined = undefined;
     if (window.activeTextEditor) {
       // 2:2-12:2|19:0-19:0
       selections = window.activeTextEditor.selections.reduce((acc, cur) => {
         const tmp = acc ? `${acc}|` : '';
         return `${tmp}${cur.start.line}:${cur.start.character}-${cur.end.line}:${cur.end.character}`;
       }, '');
+
+      // use the first line selection for building an anker for the target URL
+      if (window.activeTextEditor.selections.length) {
+        startAnker = window.activeTextEditor.selections[0].start.line + 1;
+        endAnker = window.activeTextEditor.selections[0].end.line + 1;
+      }
     }
 
     let activeFileName = '';
@@ -42,7 +50,7 @@ export class ReviewCommentService {
       console.log('Not in a git repository. Leaving SHA empty', error);
     }
 
-    const remoteUrl = this.remoteUrl(sha, activeFileName);
+    const remoteUrl = this.remoteUrl(sha, activeFileName, startAnker, endAnker);
 
     fs.appendFileSync(
       this.reviewFile,
@@ -54,7 +62,14 @@ export class ReviewCommentService {
 
   removeComment(/*lineOrLines, comment*/) {}
 
-  private remoteUrl(sha: string, filePath: string) {
+  /**
+   * Build the remote URL
+   * @param sha a git SHA that's included in the URL
+   * @param filePath the relative file path
+   * @param start the first line from the first selection
+   * @param end the last line from the first selection
+   */
+  private remoteUrl(sha: string, filePath: string, start?: number, end?: number) {
     const baseUrl = workspace.getConfiguration().get('code-review.baseUrl') as string;
     if (!baseUrl) {
       return '';
@@ -62,7 +77,8 @@ export class ReviewCommentService {
       const baseUrlWithoutTrailingSlash = baseUrl.replace(/\/$/, '');
       const filePathWithoutLeadingAnfTrailingSlash = filePath.replace(/^\/|\/$/g, '');
       const shaPart = sha ? `${sha}/` : '';
-      return `${baseUrlWithoutTrailingSlash}/${shaPart}${filePathWithoutLeadingAnfTrailingSlash}`;
+      const ankerPart = start && end ? `#L${start}-L${end}` : '';
+      return `${baseUrlWithoutTrailingSlash}/${shaPart}${filePathWithoutLeadingAnfTrailingSlash}${ankerPart}`;
     }
   }
 
