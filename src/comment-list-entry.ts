7bc18845cb59cf671052686d314b9f3d7ddb1b8c
Danny Koppenhagen <mail@d-koppenhagen.de>
2022-10-19 22:01:51 +0200

    fix: handle description line breaks in comment list
    closes #142, #143

--- src/comment-list-entry.ts
+++ src/comment-list-entry.ts
@@ -7,8 +7,8 @@ export class CommentListEntry extends TreeItem {
   constructor(
     public readonly id: string,
     public readonly label: string,
-    public readonly text: string,
-    public readonly hoverLabel: string,
+    public readonly text?: string,
+    public readonly hoverLabel?: string,
     public readonly collapsibleState: TreeItemCollapsibleState,
     public readonly data: ReviewFileExportSection,
     public readonly prio?: number,
@@ -18,10 +18,10 @@ export class CommentListEntry extends TreeItem {
   }
 
   get tooltip(): string {
-    return this.hoverLabel;
+    return this.hoverLabel ? `${this.label}\n\n${this.hoverLabel}` : this.label;
   }
 
   get description(): string {
-    return this.text || '';
+    return this.text ? this.text.replace(/[\r\n\x0B\x0C\u0085\u2028\u2029]+/g, ' ') : '';
   }
 }
